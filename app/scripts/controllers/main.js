25dd2089cc61287de3ae4e0a6532d0fa89510109
eslricardo <eslricardo@gmail.com>
2017-01-26 17:11:13 -0400

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name truextendTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the truextendTestApp
+ */
+angular.module('truextendTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
